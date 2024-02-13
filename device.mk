5c63fa2209c2360bf776ae01e216268f41e838f7
wodanesdag <wodanesdag@proton.me>
2024-02-13 16:02:24 +0700

    Initial miuicamera for ruby

---
+++ device.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/miuicamera-ruby/miuicamera-ruby-vendor.mk)
+
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Shims
+PRODUCT_PACKAGES += \
+    libgui_shim_miuicamera
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
